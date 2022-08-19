6ecb1ec764c9159a1e5c0a83b2c03855cbdc0af8
NzolaFrancoiseFifi <nzolafifi@gmail.com>
2022-08-19 01:34:26 +0100

    update2

---
+++ Sessão3/strings.py
@@ -0,0 +1,20 @@
+frase = 'O amor vence sempre'
+print(frase)
+
+email = '''
+S'''
+print('-='*10)
+txt = 'Hello World'
+
+print(txt[5])
+
+print('-='*10)
+
+num1 = '2'
+num2 = '3'
+print(f'{num1}+{num2}={num1+num2}')
+
+
+
+print('-='*10)
+print('-='*10)
